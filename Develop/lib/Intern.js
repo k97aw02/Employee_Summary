559ac5d43f177b2185642dd31b8a0f2dd96be165
k97aw02 <k97aw02@gmail.com>
2020-07-17 14:27:01 -0500

    initial commit

---
+++ Develop/lib/Intern.js
@@ -0,0 +1,13 @@
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+require ("Employee.js");
+
+class Intern extends Employee {
+    constructor (name, id, email, school){
+        super(name, id, email);
+        this.school = school;
+        this.role = "Intern";
+    }
+    getSchool(){
+        return this.school;
+    }
+}
